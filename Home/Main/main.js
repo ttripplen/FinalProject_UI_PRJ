51c8afe3698a81503d75affb0b1a58b0b17642a8
nhunguyenenn <nguyentnnse170598@fpt.edu.vn>
2024-03-19 00:08:53 +0700

    add file to project

---
+++ Home/Main/main.js
@@ -0,0 +1,40 @@
+let search = document.querySelector('.search-box');
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+}
+
+let cart = document.querySelector('.cartTab');
+document.querySelector('#cart').onclick = () => {
+    cart.classList.toggle('active');
+}
+
+let close = document.querySelector('.close');
+document.querySelector('#close').onclick = () => {
+    cart.classList.toggle('active');
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Lấy ra các span tăng và giảm số lượng sản phẩm
+    var decreaseButtons = document.querySelectorAll('.decrease-btn');
+    var increaseButtons = document.querySelectorAll('.increase-btn');
+
+    // Lặp qua từng span giảm và thêm sự kiện click
+    decreaseButtons.forEach(function(span) {
+        span.addEventListener('click', function() {
+            var quantityElement = span.nextElementSibling; // Lấy phần tử span chứa số lượng
+            var currentQuantity = parseInt(quantityElement.textContent); // Lấy số lượng hiện tại
+            if (currentQuantity > 0) {
+                quantityElement.textContent = currentQuantity - 1; // Giảm số lượng đi 1
+            }
+        });
+    });
+
+    // Lặp qua từng span tăng và thêm sự kiện click
+    increaseButtons.forEach(function(span) {
+        span.addEventListener('click', function() {
+            var quantityElement = span.previousElementSibling; // Lấy phần tử span chứa số lượng
+            var currentQuantity = parseInt(quantityElement.textContent); // Lấy số lượng hiện tại
+            quantityElement.textContent = currentQuantity + 1; // Tăng số lượng đi 1
+        });
+    });
+});
