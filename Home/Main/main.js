fca3b54f152d6fa35caace62c9a0778da71def7c
nhunguyenenn <nguyentnnse170598@fpt.edu.vn>
2024-03-20 02:38:12 +0700

    add to HomePage

--- Home/Main/main.js
+++ Home/Main/main.js
@@ -38,3 +38,18 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+}
